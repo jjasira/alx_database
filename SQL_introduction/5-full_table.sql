1902b9f3202c3e68790fe5793923097ec54d336c
James Asira <jjasira2016@gmail.com>
2024-01-10 17:15:10 +0300

    revision

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,3 +1,8 @@
 -- how to print all the information about a table
 
-SELECT * FROM first_table;
+
+
+-- Get table information
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM information_schema.COLUMNS
+WHERE TABLE_NAME = 'first_table';
