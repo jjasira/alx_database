278dc69ec65b6d3a8f5c24aa56737db09a65c8ca
James Asira <jjasira2016@gmail.com>
2024-01-10 17:29:08 +0300

    revision

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -6,4 +6,4 @@
 SELECT COLUMN_NAME AS 'Field', COLUMN_TYPE AS 'Type', IS_NULLABLE AS 'Null', COLUMN_KEY AS 'Key',
        COLUMN_DEFAULT AS 'Default', EXTRA
 FROM information_schema.COLUMNS
-WHERE TABLE_SCHEMA = 'your_database_name' AND TABLE_NAME = 'first_table';
+WHERE TABLE_NAME = 'first_table';
