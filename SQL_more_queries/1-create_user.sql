3b34bb7bf64bddd4733a9906936f065917467dc0
James Asira <jjasira2016@gmail.com>
2024-01-11 09:31:19 +0300

    revision

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,10 +1,8 @@
--- How to create a new user
-
-CREATE USER IF NOT EXIST 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
+-- Create user only if it doesn't exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
 -- Grant all privileges to the user
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 
 -- Flush privileges to apply changes
--- FLUSH PRIVILEGES;
+FLUSH PRIVILEGES;
