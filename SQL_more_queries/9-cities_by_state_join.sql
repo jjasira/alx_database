a89f89983b764ee92c4f7259734d118a7a74e77f
James Asira <jjasira2016@gmail.com>
2024-01-11 10:04:33 +0300

    revision

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,13 @@
 -- List all cities with their id, name, and corresponding state name sorted by cities.id
+-- SELECT cities.id, cities.name, states.name AS state_name
+-- FROM cities, states
+-- WHERE cities.state_id = states.id
+-- ORDER BY cities.id ASC;
+
+-- using joins
+
+-- List all cities with their id, name, and corresponding state name sorted by cities.id using JOIN
 SELECT cities.id, cities.name, states.name AS state_name
-FROM cities, states
-WHERE cities.state_id = states.id
+FROM cities
+JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
