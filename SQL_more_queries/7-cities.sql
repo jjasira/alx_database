e2ff6e170f879052f921c92b5f57ec6e0dd367ff
James Asira <jjasira2016@gmail.com>
2024-01-11 09:53:48 +0300

    commit for task 8

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,13 @@
+-- Check if the database exists before creating
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Use the database
+USE hbtn_0d_usa;
+
+-- Check if the table exists before creating
+CREATE TABLE IF NOT EXISTS cities (
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id)
+);
